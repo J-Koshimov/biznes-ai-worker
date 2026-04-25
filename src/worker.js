a472418f7289caa43e3ed76b1ba1b4ce0b5bea3a
J-Koshimov <usercool8591@gmail.com>
2026-04-25 23:33:22 +0500

    Update worker.js

--- src/worker.js
+++ src/worker.js
@@ -10,7 +10,7 @@ export default {
     if (request.method === "GET") {
       try {
         const apiRes = await fetch(
-          `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${env.GEMINI_API_KEY}`,
+          `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${env.GEMINI_API_KEY}`,
           {
             method: "POST",
             headers: {
